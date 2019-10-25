d9081b48ab579c8831c712a963ee237181df5b98
loicbtd <loic.bert.marcel@gmail.com>
2019-10-25 08:15:33 -0400

    la persistence des données fonctionne

--- donnee/DevoirDAO.js
+++ donnee/DevoirDAO.js
@@ -1,20 +1,47 @@
 var DevoirDAO = function() {
-    var listeDevoir = [
-        new Devoir("nom0", "matiere0", "description0", 0),
-        new Devoir("nom1", "matiere1", "description1", 1),
-        new Devoir("nom2", "matiere2", "description2", 2)
-    ];
+
+    var listeDevoir = null;
+
+    var initialiser = function() {
+        if(!listeDevoir) {
+            listeDevoir = [];
+        }
+    }
 
     this.lister = function() {
+        if(localStorage['devoir']) {
+            listeDevoir = JSON.parse(localStorage['devoir']);
+        }
+
+        for(position in listeDevoir) {
+            var devoir = new Devoir(
+                listeDevoir[position].nom,
+                listeDevoir[position].matiere,
+                listeDevoir[position].description,
+                listeDevoir[position].id
+            );
+
+            listeDevoir[position] = devoir;
+        }
+
         return listeDevoir;
     }
 
     this.ajouter = function(devoir) {
-        devoir.id = listeDevoir.length;
-        listeDevoir.push(devoir);
+        if(listeDevoir.length > 0)
+            devoir.id = listeDevoir[listeDevoir.length-1].id + 1;
+        else
+            devoir.id = 0;
+        
+        listeDevoir[devoir.id] = devoir;
+        localStorage['devoir'] = JSON.stringify(listeDevoir);
     }
 
-    this.modifier = function(devoir) {
+
+    this.modifier = function(devoir) {        
         listeDevoir[devoir.id] = devoir;
+        localStorage['devoir'] = JSON.stringify(listeDevoir);
     }
+
+    initialiser();
 }
