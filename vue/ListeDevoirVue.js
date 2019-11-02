4ca4bdda0352cb755f920db4657eb7f51f0b61c1
loicbtd <loic.bert.marcel@gmail.com>
2019-11-02 15:54:37 -0400

    ajout du framework materialize et début d'amélioration du gui

--- vue/ListeDevoirVue.js
+++ vue/ListeDevoirVue.js
@@ -9,13 +9,18 @@ var ListeDevoirVue = (function() {
             var listeDevoir = document.getElementById("liste-devoir");
 
             var textLi="";
+
             for (const numeroDevoir in listeDevoirDonnee) {
-               textLi += '<li><a href="#devoir/' + listeDevoirDonnee[numeroDevoir].id + '">' +
-               listeDevoirDonnee[numeroDevoir].nom +
-               "</a>" + " " +
-               '<a href="#modifier-devoir/' + listeDevoirDonnee[numeroDevoir].id + '">' +
-               "modifier" +
-               "</a></li>";
+               textLi += '<li class="collection-item avatar">' +
+               '<a href="#devoir/' + listeDevoirDonnee[numeroDevoir].id + '">' + 
+               '<img src="ressource/image/img_book.png" alt="" class="circle">' +
+               '<span class="title">' + listeDevoirDonnee[numeroDevoir].nom + '</span>' +
+               '<p>' + listeDevoirDonnee[numeroDevoir].matiere + '</p>' +
+               '</a>' +
+               '<a class="secondary-content" href="#modifier-devoir/' + listeDevoirDonnee[numeroDevoir].id + '">' +
+               '<i class="far fa-edit"></i>' +
+               '</a>' + 
+               "</li>";
             }
             listeDevoir.innerHTML = textLi;
         }
